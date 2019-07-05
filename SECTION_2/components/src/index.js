55c2cd32347f668a865d4002391eb7447f1646d3
B-Musick <bmuze1@gmail.com>
2019-07-05 11:14:40 -0600

    faker library, avatar image added

--- SECTION_2/components/src/index.js
+++ SECTION_2/components/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar"/>
+                    {/* Reference faker library for avatar image */}
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
