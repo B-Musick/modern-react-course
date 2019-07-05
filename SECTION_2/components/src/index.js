b368a39f0de66b5ecab30452290cf9d099df8f35
B-Musick <bmuze1@gmail.com>
2019-07-05 11:03:55 -0600

    Added blog post outline

---
+++ SECTION_2/components/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Brendan
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
