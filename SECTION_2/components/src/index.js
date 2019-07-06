f2c5c4fbb1d7026a34e8bd8cde5cc918fb2aad9d
B-Musick <bmuze1@gmail.com>
2019-07-06 15:08:07 -0600

    Section 3 complete

--- SECTION_2/components/src/index.js
+++ SECTION_2/components/src/index.js
@@ -8,10 +8,34 @@ const App = () => {
     return (
         // Semantic-UI Comment outline
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="Brendan" timeAgo="Today at 4:45PM" comment="I like you" image={faker.image.avatar()}/>
-            <CommentDetail author="Adam" timeAgo="Today at 3:45PM" comment="I hate you" image={faker.image.avatar()}/>
-            <CommentDetail author="Davis" timeAgo="Today at 2:45PM" comment="I dig you" image={faker.image.avatar()}/>
+            <ApprovalCard>
+                {/* This component will exist on the props object */}
+                <CommentDetail 
+                    author="Brendan" 
+                    timeAgo="Today at 4:45PM" 
+                    comment="I like you" 
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Adam"
+                    timeAgo="Today at 3:45PM"
+                    comment="I hate you"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Davis"
+                    timeAgo="Today at 2:45PM"
+                    comment="I dig you"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+
+                
         </div>
     );
 };
