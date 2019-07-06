90eea6c7d5aef070a16db0bf66ee864629497e18
B-Musick <bmuze1@gmail.com>
2019-07-06 14:21:31 -0600

    ApprovalCard component made, connected to index

--- SECTION_2/components/src/index.js
+++ SECTION_2/components/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail'
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         // Semantic-UI Comment outline
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author="Brendan" timeAgo="Today at 4:45PM" comment="I like you" image={faker.image.avatar()}/>
             <CommentDetail author="Adam" timeAgo="Today at 3:45PM" comment="I hate you" image={faker.image.avatar()}/>
             <CommentDetail author="Davis" timeAgo="Today at 2:45PM" comment="I dig you" image={faker.image.avatar()}/>
