4108fe7c3e6000e167d6c3ef7a1de0f58e1ff0cf
B-Musick <bmuze1@gmail.com>
2019-07-08 11:11:02 -0600

    icon, season text, getSeason method

--- SECTION_4/seasons/src/SeasonDisplay.js
+++ SECTION_4/seasons/src/SeasonDisplay.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return (
-        <div>Season Display</div>
-    )
+// Function to determine season
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        // If in summer months and check if in northern or southern hemisphere with ternary
+        return lat > 0 ? 'summer' : 'winter'
+    }else{
+        return lat < 0 ? 'winter' : 'summer'
+    }
+}
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burr, it is chill' : 'Lets hit the beach';
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    
+    
+    return <div>
+        <i className={`${icon}`} />
+        <h1>{text}</h1>
+        <i className={`${icon}`} />
+        
+    </div>
+    
 }
 
 export default SeasonDisplay;
--- SECTION_4/seasons/src/index.js
+++ SECTION_4/seasons/src/index.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-    constructor(props){
-        // Specially named, belongs to JS but not mandatory
-        // Called everytime new instance of app called
-        // Way to initialize state
-        super(props); // Call React.Component constructor function (mandatory)
+    // constructor(props){
+    //     // Specially named, belongs to JS but not mandatory
+    //     // Called everytime new instance of app called
+    //     // Way to initialize state
+    //     super(props); // Call React.Component constructor function (mandatory)
 
-        this.state = {
-            // Want to initialize latitude here
-            // Default to sensible value
-            lat: null, // Dont know latitude yet
-            errorMessage: '' 
-        }
+    //     this.state = {
+    //         // Want to initialize latitude here
+    //         // Default to sensible value
+    //         lat: null, // Dont know latitude yet
+    //         errorMessage: '' 
+    //     }
+
+    // }
+
+    // Constructor will automatically be called as well as super(props)
+    state = { lat: null, errorMessage: ''}
+
+    componentDidMount(){
+        // Put initial data loading requests here for best practice
         // Dont want to initialize in render() since render called all the time
         window.navigator.geolocation.getCurrentPosition(
             // Call back when gets user location (success callback)
             // Use this to determine if in northern or southern hemisphere
-            (position) => {
-                // Want to use latitude alone and set the state
-                this.setState({ lat: position.coords.latitude})
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            (position) => this.setState({ lat: position.coords.latitude })
+                // Want to use latitude alone and set the state  
+            ,
+            (err) => this.setState({ errorMessage: err.message })
+            
         );
-    }
+    };
 
+    componentDidUpdate() {
+        console.log('My component did update')
+    }
     render() {
         // Render is required for every component otherwise get error
         // Error will be shown if error occurs, update state
@@ -36,7 +46,9 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            // Take property from state, and pass it to the SeasonDisplay component to 
+            // display it
+            return <SeasonDisplay lat={this.state.lat}/>
         }
         
         return <div>Loading!</div>
