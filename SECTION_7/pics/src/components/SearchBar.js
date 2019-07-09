bc9f5a9b3c80d877e097e44126147f12dd4b1370
B-Musick <bmuze1@gmail.com>
2019-07-09 10:27:03 -0600

    handled input

---
+++ SECTION_7/pics/src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
+
+    render(){
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" onChange={(e)=>console.log(e.target.value)} />
+                    </div>
+                    
+                </form>
+
+            </div>
+        )
+
+    }
+}
+export default SearchBar;
