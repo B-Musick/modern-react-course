bd8bdce6576efa2891efefdc818cc1b3faa7f080
B-Musick <bmuze1@gmail.com>
2019-07-10 09:09:18 -0600

    API request gets data from unsplash using axios

--- SECTION_7/pics/src/components/App.js
+++ SECTION_7/pics/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
@@ -7,7 +8,18 @@ class App extends React.Component{
         // Pass this into the SearchBar Component which is called in render()
         // In the SearchBar Component it will be called in the 'onFormSubmit' method
         // When form submitted the term is passed into here and called
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                // Query string parameters we want to add
+                query: term
+            },
+            headers: {
+
+                Authorization: 'Client-ID d32cebea123578bd377e968593d8aa234c605a79e5f3205c19d6cbe655b400e6'
+            }
+            // Unsplash will send back JSON file with a bunch of photos
+        });
+
     }
 
     render(){
--- SECTION_7/pics/src/components/SearchBar.js
+++ SECTION_7/pics/src/components/SearchBar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    // onInputChange(event){
-    //     console.log(event.target.value);
-    // }
+    state = { term: ''}
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        // Call method coming in through props from App
+        this.props.onSubmit(this.state.term);
+    }
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                {/* When form submitted, onFormSubmit is called and term state set, then 
+                onSubmit method passed in from parent is called (term is passed to this parent
+                method) */}
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={(e)=>console.log(e.target.value)} />
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={(e)=>this.setState({term: e.target.value})} 
+                        />
                     </div>
                     
                 </form>
