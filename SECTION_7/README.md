42863d52fae7783dcdceaf211e0d340692440c9f
B-Musick <bmuze1@gmail.com>
2019-07-11 16:40:29 -0600

    pics app complete

--- SECTION_7/README.md
+++ SECTION_7/README.md
@@ -41,3 +41,32 @@ API
 - Unsplash will send back JSON records matching requests and can use to post images
 - AJAX CLient will make the request (not the React App)
 - Make get request with axios 
+
+FLOW
+- Component renders itself with no images
+- onSearchSubmit method is called
+- Request made to unsplash
+- Request complete
+- Set image data on state of App component (use setState)
+- App rerenders to show image (ImageList Component will show them)
+
+HANDLE PROMISE FROM API (ASYNCHRONOUS FUNCTION) WITH ASYNC/AWAIT
+- Use async on the method name
+- Use await keyword on the response
+
+SET APP STATE
+- Add images: [] 
+- Setstate in the onSearchSubmit , and will cause the rerender
+
+IMAGELIST
+- Call this component in App.js
+- Pass in the images found to the component through props from App component
+
+IMAGECARD COMPONENT
+- Will need component to load each individual image so we can determine the height of it and use the grid-row-end CSS property to span the certain amount of cells to separate it from its neighbour
+
+FLOW
+- Let imageCard render itself and its image
+- Rach into DOM and figure out the height of the image
+- Set the image height on state to get the component to rerender
+- When rendring addign a 'grid-row-end' to make sure the image takes up the appropriate space
--- SECTION_7/pics/src/components/ImageCard.js
+++ SECTION_7/pics/src/components/ImageCard.js
@@ -2,13 +2,38 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {spans:0}
+        // Assign to instance variable so can refer to it later
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        // When image loads, it emits this 'load' event that will let us know
+        // when loaded
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        // Used to determine the height value
+        const height = this.imageRef.current.clientHeight;
+        //  Figure out how many rows to be 
+        const spans = Math.ceil(height / 10 + 1);
+        // Set the spans which
+        this.setState({
+            spans
+        })
+    }
     render(){
         // Destructuring code
         const {description, urls } = this.props.image
         return (
-            <div>
+            // Set the style of the grid-row-end to the state
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
                 {/* Pass a prop of our image object */}
                 <img 
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
